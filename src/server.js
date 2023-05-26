9bba54e289538b682f01f8f1e22b58b785820f02
Marcelo Cavassani <marceloscavassani@gmail.com>
2023-05-25 23:48:01 -0300

    add e finalizando buffers

--- src/server.js
+++ src/server.js
@@ -1,18 +1,23 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js';
 const users = []
-const server = http.createServer((request, response) =>{
+
+const server = http.createServer(async (request, response) =>{
     const { method, url} = request
+
+    await json(request, response)
+
+
     if( method == 'GET' && url == '/users'){
-        return response
-        .setHeader('Content-type','application/json')
-        .end(JSON.stringify(users))
+        return response.end(JSON.stringify(users))
     }
 
     if( method == 'POST' && url == '/users'){
+        const { name , email } = request.body
         users.push({
             id: 1,
-            nome: "john jones",
-            email: "joaum@gmail.com"
+            name,
+            email
         })
         return response.writeHead(201).end()
     }   
